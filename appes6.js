3077b7db0e4e69479fcae0a38a25c3b946d60ba9
mdtrunzo <31319089+mdtrunzo@users.noreply.github.com>
2019-07-09 11:12:29 +1000

    Update appes6.js

--- appes6.js
+++ appes6.js
@@ -9,9 +9,8 @@ class Book {
 class UI {
   addBookToList(book) {
     const list = document.getElementById('book-list');
-    // Create tr element
     const row = document.createElement('tr');
-    // Insert cols
+
     row.innerHTML = `
       <td>${book.title}</td>
       <td>${book.author}</td>
@@ -23,20 +22,13 @@ class UI {
   }
 
   showAlert(message, className) {
-    // Create div
     const div = document.createElement('div');
-    // Add classes
     div.className = `alert ${className}`;
-    // Add text
     div.appendChild(document.createTextNode(message));
-    // Get parent
     const container = document.querySelector('.container');
-    // Get form
     const form = document.querySelector('#book-form');
-    // Insert alert
     container.insertBefore(div, form);
 
-    // Timeout after 3 sec
     setTimeout(function () {
       document.querySelector('.alert').remove();
     }, 3000);
@@ -96,56 +88,40 @@ class Store {
   }
 }
 
-// DOM load event
 document.addEventListener('DOMContentLoaded', Store.displayBooks)
 
-// Event Listener for add book
 document.getElementById('book-form').addEventListener('submit', function (e) {
-  // Get form values
   const title = document.getElementById('title').value,
     author = document.getElementById('author').value,
     isbn = document.getElementById('isbn').value
 
-  // Instantiate book
   const book = new Book(title, author, isbn);
 
-  // Instantiate UI
   const ui = new UI();
 
-  // Validate
   if (title === '' || author === '' || isbn === '') {
-    // Error alert
     ui.showAlert('Please fill in all fields', 'error');
   } else {
-    // Add book to list
     ui.addBookToList(book);
 
-    // Add to LS
     Store.addBook(book)
 
-    // Show success
     ui.showAlert('Book Added!', 'success');
-
-    // Clear fields
+    
     ui.clearFields();
   }
 
   e.preventDefault();
 });
 
-// Event Listener for delete
-document.getElementById('book-list').addEventListener('click', function (e) {
 
-  // Instantiate UI
+document.getElementById('book-list').addEventListener('click', function (e) {
   const ui = new UI();
 
-  // Delete book
   ui.deleteBook(e.target);
 
-  // Remove from LS
   Store.removeBook(e.target.parentElement.previousElementSibling.textContent)
 
-  // Show message
   ui.showAlert('Book Removed!', 'success');
 
   e.preventDefault();
